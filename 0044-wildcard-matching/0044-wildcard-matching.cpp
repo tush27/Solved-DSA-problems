cffd93514a17a0171b3af9abdb7ae61a2241fe30
Tushar Saini <96056398+tush27@users.noreply.github.com>
2022-11-19 23:56:00 +0530

    Time: 196 ms (16.16%), Space: 11.3 MB (63.03%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,28 +1,4 @@
 class Solution {
-     bool helper(string s,string p,int i,int j,vector<vector<int>>&dp){
-        if(j<0 && i>-1)
-            return false;
-         if(i==-1 && j==-1)
-             return true;
-        if(i<0){
-            while(j>-1 && p[j]=='*')
-                j--;
-            if(j<0)
-                return true;
-            return false;        
-        }
-         if(dp[i][j]!=-1)
-             return dp[i][j];
-  
-        if(s[i]==p[j] || (p[j]=='?' && s[i])){
-            return dp[i][j]= helper(s,p,i-1,j-1,dp);
-        }
-        else if(p[j]=='*'){
-            return dp[i][j]= helper(s,p,i,j-1,dp) || helper(s,p,i-1,j,dp);
-        }
-        return dp[i][j]= false;
-        
-    }
 public:
     bool isMatch(string s, string p) {
         int n=s.size();
@@ -42,8 +18,7 @@ public:
             dp[0][j]=false;
             j++;
         }
-        dp[0][0]=true;
-        
+        dp[0][0]=true;        
         for(int i=1;i<n+1;i++)
             for(int j=1;j<m+1;j++){
                 if(s[i-1]==p[j-1] || (p[j-1]=='?' && s[i-1])){
@@ -55,10 +30,6 @@ public:
                 else
                 dp[i][j]=false;
             }
-        
         return dp[n][m];  
-        
-        
-
     }
 };
