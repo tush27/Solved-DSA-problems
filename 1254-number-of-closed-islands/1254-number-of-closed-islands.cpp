a86f9a62dd499b2539f4c07e127c6fc2664b2144
Tushar Saini <96056398+tush27@users.noreply.github.com>
2023-04-06 20:31:04 +0530

    Time: 17 ms (37.16%), Space: 14.3 MB (10.83%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,32 +1,32 @@
 class Solution {
-    void bfs(vector<vector<int>>&grid,vector<vector<int>>&visited,int x,int y,int n,int m){
+    void bfs(vector<vector<int>>&grid,vector<vector<bool>>&visited,int x,int y,int n,int m){
          queue<pair<int,int>>q;
         q.push({x,y});
-        visited[x][y]=1;
+        visited[x][y]=true;
         while(!q.empty()){
             auto front=q.front();
             q.pop();
             int i=front.first;
             int j=front.second;
         //top
-            if(i-1 >=0 && grid[i-1][j]==0 && visited[i-1][j]==0){
+            if(i-1 >=0 && grid[i-1][j]==0 && visited[i-1][j]==false){
                 q.push({i-1,j});
-                visited[i-1][j]=1;
+                visited[i-1][j]=true;
             }
             //bottom
-            if(i+1 <n && grid[i+1][j]==0 && visited[i+1][j]==0){
+            if(i+1 <n && grid[i+1][j]==0 && visited[i+1][j]==false){
                 q.push({i+1,j});
-                visited[i+1][j]=1;
+                visited[i+1][j]=true;
             }
             //left
-            if(j-1 >=0 && grid[i][j-1]==0 && visited[i][j-1]==0){
+            if(j-1 >=0 && grid[i][j-1]==0 && visited[i][j-1]==false){
                 q.push({i,j-1});
-                visited[i][j-1]=1;
+                visited[i][j-1]=true;
             }
             //right
-            if(j+1 <m && grid[i][j+1]==0 && visited[i][j+1]==0){
+            if(j+1 <m && grid[i][j+1]==0 && visited[i][j+1]==false){
                 q.push({i,j+1});
-                visited[i][j+1]=1;
+                visited[i][j+1]=true;
             }        
         }
     }
@@ -34,25 +34,25 @@ public:
     int closedIsland(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>visited(n,vector<int>(m,0));
+        vector<vector<bool>>visited(n,vector<bool>(m,false));
         int ans=0;
         //top-bottom
         for(int i=0;i<n;i++){
-            if(grid[i][0] == 0 && visited[i][0]==0)
+            if(grid[i][0] == 0 && visited[i][0]==false)
                 bfs(grid,visited,i,0,n,m);
-            if(grid[i][m-1] == 0 && visited[i][m-1]==0)
+            if(grid[i][m-1] == 0 && visited[i][m-1]==false)
                 bfs(grid,visited,i,m-1,n,m);
         }
         //left-right
         for(int i=0;i<m;i++){
-            if(grid[0][i] == 0 && visited[0][i]==0)
+            if(grid[0][i] == 0 && visited[0][i]==false)
                 bfs(grid,visited,0,i,n,m);
-            if(grid[n-1][i] == 0 && visited[n-1][i]==0)
+            if(grid[n-1][i] == 0 && visited[n-1][i]==false)
                 bfs(grid,visited,n-1,i,n,m);
         }
         for(int i=1;i<n-1;i++){
             for(int j=1;j<m-1;j++){
-                if(grid[i][j]==0 && visited[i][j]==0){
+                if(grid[i][j]==0 && visited[i][j]==false){
                     bfs(grid,visited,i,j,n,m);
                     ans++;
                 }
